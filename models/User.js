10ebc686a3b7e9209f5892674621719d42d44e64
ikram207 <ikrambdk55@gmail.com>
2025-08-10 15:11:04 +0100

    4eme commit

--- models/User.js
+++ models/User.js
@@ -5,14 +5,20 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-}, { timestamps: true });
+}, {
+  timestamps: true,
+});
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
+// Méthode pour comparer le password entré avec le hash en base
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+// Middleware avant sauvegarde : hash du password si modifié
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, authUser } = require('../controllers/userController');
 
+// Inscription à POST /api/users
 router.post('/register', registerUser);
+
+// Connexion à POST /api/users/login
 router.post('/login', authUser);
 
 module.exports = router;
